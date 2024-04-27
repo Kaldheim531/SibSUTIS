e71f195ebf29a3c8a5e7a9f021295f797a5e04e7
Kaldheim531 <149830173+Kaldheim531@users.noreply.github.com>
2024-04-27 23:59:51 +0700

    Update lab12.c

--- 2-semestr/lab12/lab12.c
+++ 2-semestr/lab12/lab12.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*Создайте структуру Вектор с элементами x, y, z – декартовые координаты.
+Определить для M заданных векторов длину каждого вектора и найти номер самого длинного вектора.*/
 
+struct vector
+{
+    int x;
+    int y;
+    int z;
+};
+int ThemostbiggestVector(struct vector)
+{
+    return  0;
+}
+int main()
+{   
+    srand(time(NULL));
+    int M=3;
+    struct vector number[M];
+    for(int i=0;i<M;i++)
+    {
+        struct vector number[i]={rand()%5,rand()%5,rand()%5};
+    }
+    
+     for(int i=0;i<M;i++)
+    {
+        printf("%d \n",number[i].x);
+    }
+    
+
+}
