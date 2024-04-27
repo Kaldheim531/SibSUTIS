2419ae9a1b48dbd98450b05d041db8855d2efac8
Kaldheim531 <149830173+Kaldheim531@users.noreply.github.com>
2024-04-27 23:49:31 +0700

    Update lab11-1.c

--- 2-semestr/lab11-1/lab11-1.c
+++ 2-semestr/lab11-1/lab11-1.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include<math.h>
+#define E 0.001
 
+double arccos(int n,double x)
+{
+    if (fabs(x)<=1)
+    {
+        double result= M_PI_2 -((2*n)/(pow(2,2*n)*pow(n,2))*(pow(x,2*n+1)/(2*n+1)));
+
+
+        if (fabs(result) < E)
+        {
+            return 0;
+        }
+    return result + arccos(n+1, x);
+
+    }
+    return 0;
+}
+
+int main()
+{
+   double x = 0.5;
+   int n =10;
+   printf("arccos(%f) = %f\n", x, arccos(n,x));
+   printf("arccos %f",acos(x));
+    return 0;
+}
